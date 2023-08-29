f26a0053628eb31a8315f02f24be15974b927364
Grady <gradyramberg@gmail.com>
2023-08-29 15:00:35 -0700

    Fixed Writer and Reader

--- Reader.java
+++ Reader.java
@@ -1,14 +1,36 @@
-public class Reader
-{
-    String word = "";
-    public String read (wording file) 
-    {
-        BufferedReader br = new BufferedReader (new FileReader(file));
-		while (br.ready())
-        {
-            word += br.read();
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Reader {
+    public static void main(String[] args) throws IOException {
+        write("Hello", "file.txt");
+        System.out.print(read("file.txt"));
+    }
+
+    public static String read(String fileName) {
+        try {
+            String word = "";
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            while (br.ready()) {
+                word += (char) br.read();
+            }
+            br.close();
+            return word;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-		br.close();
-        return word;
+        return "";
+    }
+
+    public static void write(String str, String fileName) {
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            writer.write(str);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
